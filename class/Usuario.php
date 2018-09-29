217796affc07984ee865a73bed075bdb8a321bfc
wesley17 <wesley_pereira12@yahoo.com>
2018-09-29 18:13:54 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -47,13 +47,52 @@ class Usuario {
 		if (count($results) > 0) {
 
 			$row = $results[0];
-			
+
 			$this->setIdusuario($row['idusuario']);
 			$this->setDeslogin($row['deslogin']);
 			$this->setDessenha($row['dessenha']);
 			$this->setDtcadastro(new Datetime($row['dtcadastro']));
 			
 		}
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	public static function search($login){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ", array(':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($Login, $password){
+
+		$sql = new Sql();
+		
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$Login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0) {
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new Datetime($row['dtcadastro']));			
+		} else{
+
+			throw new Exception("Login ou senha invalidos.");
+
+		}
+
 	}
 
 	public function __toString(){
--- index.php
+++ index.php
@@ -10,9 +10,24 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
-$jose = new Usuario();
-$jose->loadbyId(3);
+//Carrega um unico usuário
+//$jose = new Usuario();
+//$jose->loadbyId(6);
+//echo $jose;
 
-echo $jose;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("Ma");
+//echo json_encode($search);
+
+//Carrega um usuário usando um login ou uma senha
+
+$usuario = new Usuario();
+$usuario->login("Malu", "654");
+
+echo $usuario;
 
 ?>
